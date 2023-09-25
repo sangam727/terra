79a09fd34f1a52d756398ddea4770337f37d9cfe
sangam727 <shewale.sangam@gmail.com>
2023-09-26 02:30:47 +0530

    map veriable is added

--- terraform_variables_details/createInstace.tf
+++ terraform_variables_details/createInstace.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "MyFirstInstance" {
-    ami = "ami-053b0d53c279acc90"
+    ami = lookup(var.AMIS,var.AWS_REGION)
     instance_type = "t2.micro"
 
     tags = {
--- terraform_variables_details/variables.tf
+++ terraform_variables_details/variables.tf
@@ -10,3 +10,13 @@ variable "Security_Group" {
   type = list
   default = ["sg-1234","sg-4567","sg-78910"]
 }
+
+variable "AMIS" {
+    type = map
+    default = {
+        us-east-1 = "ami-0f6426fe0b6419ecc"
+        us-east-2 = "ami-0b6968e5c7117349a"
+        us-west-1 = "ami-092efbcc9a2d2be8a"
+        us-west-2 = "ami-08ade17b00c7e2172"
+    }
+}
